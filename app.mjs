230fe5776670f9136c80ec663f5f970d7c879772
asakshat <asakshat453@gmail.com>
2024-04-27 00:38:43 +0200

    test

--- app.mjs
+++ app.mjs
@@ -2,13 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userAuth from './routes/userAuth.mjs';
 import { authenticateToken } from './middlewares/authenticate.mjs';
-i = dotenv.config();
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
-console.log(user);
 
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
