e6734952bd00050781f0085d3bfc2111e44646fa
asakshat <asakshat453@gmail.com>
2024-04-29 10:00:23 +0200

    fixed imports

--- routes/userRoute.mjs
+++ routes/userRoute.mjs
@@ -1,13 +1,8 @@
 import express from 'express';
-import {
-	loginUser,
-	signUpUser,
-	leaveGroup,
-} from '../controllers/userController.mjs';
+import { loginUser, signUpUser } from '../controllers/userController.mjs';
 const router = express.Router();
 
 router.post('/login', loginUser);
 router.post('/signup', signUpUser);
-router.post('/joingroup/:group_id', joinGroup);
-router.post('/leavegroup/:group_id', leaveGroup);
+
 export default router;
