4c9847a3cb9a669480c0425f27e688bff963c2b4
asakshat <asakshat453@gmail.com>
2024-04-29 10:36:18 +0200

    fixed imports

--- routes/directMessageRoute.mjs
+++ routes/directMessageRoute.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { sendDirectMessage } from '../controllers/directMessageContoller.mjs';
+import { sendDirectMessage } from '../controllers/directMessageController.mjs';
 
 router.post('/send/:sender_id/:receiver_id', sendDirectMessage);
 
