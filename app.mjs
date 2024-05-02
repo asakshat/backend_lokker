915387fcc081b669063393ff4b6206b74bf2d24e
asakshat <asakshat453@gmail.com>
2024-05-02 05:01:42 +0200

    fixed get req1

--- app.mjs
+++ app.mjs
@@ -6,12 +6,6 @@ import groupRoute from './routes/groupRoute.mjs';
 import directMessageRoute from './routes/directMessageRoute.mjs';
 import { authenticateToken } from './middlewares/authenticate.mjs';
 import cors from 'cors';
-import { executeQuery } from './configs/database.mjs';
-const users = await executeQuery(
-	'SELECT DISTINCT CASE WHEN sender_id = $1 THEN receiver_id ELSE sender_id END AS user_id FROM "DirectMessage" WHERE sender_id = $1 OR receiver_id = $1',
-	['49e49736-c3eb-4c45-a446-ecff0d5ce0d5']
-);
-console.log(users);
 
 dotenv.config();
 
--- routes/directMessageRoute.mjs
+++ routes/directMessageRoute.mjs
@@ -8,6 +8,6 @@ import {
 
 router.post('/send/:sender_id/:receiver_id', sendDirectMessage);
 router.get('/get/:sender_id/:receiver_id', getDirectMessages);
-router.get('/get/:user_id', messagedUsers);
+router.get('/get/:sender_id', messagedUsers);
 
 export default router;
