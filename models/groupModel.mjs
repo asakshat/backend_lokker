1dbb2f9bcd44f4d537d943e2a600978acbbbcc78
asakshat <asakshat453@gmail.com>
2024-05-03 11:19:32 +0200

    done

--- models/groupModel.mjs
+++ models/groupModel.mjs
@@ -43,22 +43,10 @@ export const leaveGroupFunction = async (user_id, group_id) => {
 	return leave;
 };
 
-export const postMessageFunction = async (user_id, group_id) => {
-	const group = await executeQuery(
-		'SELECT group_id FROM "Group" WHERE group_id = $1',
-		[group_id]
-	);
-
-	const member = await executeQuery(
-		'SELECT user_id FROM "GroupMember" WHERE user_id = $1 AND group_id = $2',
-		[user_id, group_id]
-	);
-	if (member.length === 0) {
-		throw Error('Not in group');
-	}
+export const postMessageFunction = async (user_id, group_id, message) => {
 	const message = await executeQuery(
-		'INSERT INTO "Message"(user_id,group_id) VALUES ($1,$2)',
-		[user_id, group_id]
+		'INSERT INTO "Message"(user_id,group_id, message) VALUES ($1,$2,$3)',
+		[user_id, group_id, message]
 	);
 	return message;
 };
